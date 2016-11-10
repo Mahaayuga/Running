e28326ba8630fc372bd63d0aa797883e2ace08b9
Mahaayuga <benoit.mazure@gmail.com>
2016-11-11 00:32:25 +0100

    modifs mineures

--- app/controllers/code_html_slider.rb
+++ app/controllers/code_html_slider.rb
@@ -4,6 +4,8 @@ require_relative 'courses_processus'
 run = CSVReader.new
 run.mise_en_memoire("../../db/data.csv")
 
-puts run.all_case "dist"
-
+print run.all_case "vitesse"
+printf "\n"
+print run.all_case "v.to_f"
+printf "\n"
 
